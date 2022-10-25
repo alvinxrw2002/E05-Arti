4fac6bf711378a75a949aba0428b4a6c4936b1cf
stenly10 <yosuasaputra.stenly@gmail.com>
2022-10-25 17:54:01 +0700

    progress 1

---
+++ leaderboard/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from leaderboard.models import Review
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['text']
+        labels = {'text':'Input Text'}
+        widgets = {
+            'text' : forms.Textarea(attrs={'cols' : 5, 'rows' : 2})
+        }
