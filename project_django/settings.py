d9de8e504eeb7781d79b6348446802919e3942ae
alvinxrw <alvin.xavier@ui.ac.id>
2022-10-28 21:32:22 +0700

    nambahin style login sama register

--- project_django/settings.py
+++ project_django/settings.py
@@ -135,7 +135,6 @@ STATICFILES_DIRS = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
