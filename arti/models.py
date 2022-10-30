5fc844f9a299e01014728fec208dc5056c8fa142
Your Namegit <you@example.com>
2022-10-30 22:18:42 +0700

    penambahan ajax pada profile 2

--- arti/models.py
+++ arti/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -5,3 +6,12 @@ from django.contrib.auth.models import User
 class Karya(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     gambar = models.ImageField(upload_to="images/")
+    judul = models.CharField(max_length=255)
+    kategori = models.CharField(max_length=255)
+    harga = models.CharField(max_length=255)
+    deskripsi = models.CharField(max_length=10000)
+    tanggal = models.DateField(auto_now_add=True)
+
+class UserArti(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    kategori_favorit = models.CharField(max_length=255)
