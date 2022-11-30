a7ce1a4a108454d3cfa95daeeedce113c3d0c5df
alvin <alvin.xavier@ui.ac.id>
2022-11-30 17:41:14 +0700

    p

--- project_django/settings.py
+++ project_django/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-3@5wx%(^*zl68l(o$^m-3%cvjv0g&mom1ra=oj5f048_al57s7
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = [f'https://{APP_NAME}.up.railway.app']
+CSRF_TRUSTED_ORIGINS = [f'https://arti-pbp-e05.up.railway.app/']
 
 # Application definition
 
@@ -140,7 +140,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
