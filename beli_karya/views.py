b0f822d2177ee93d5252738efd813820afa9b7b9
rebekaalma <rebekabarimbing@gmail.com>
2022-12-12 20:02:03 +0700

    add json function

--- beli_karya/views.py
+++ beli_karya/views.py
@@ -5,6 +5,9 @@ from django.contrib.auth.decorators import login_required
 from arti.models import Karya
 from beli_karya.models import Transaksi
 import json
+from django.views.decorators.csrf import csrf_exempt
+from django.core import serializers
+from django.http.request import QueryDict
 
 # Create your views here.
 def get_karyas(request):
@@ -39,3 +42,33 @@ def beli_karya(request):
             transaksi = Transaksi(user=loggedin_user, karya = k)
             transaksi.save()
     return render(request, 'beli-karya.html', context)
+
+@csrf_exempt
+def get_karya_json(request):
+    karyas = Karya.objects.all()
+    lst = []
+    for karya in karyas:
+        transaksi = Transaksi.objects.filter(karya = karya)
+        lst.append({
+            'id': karya.pk,
+            'judul': karya.judul,
+            'kategori': karya.kategori,
+            'deskripsi': karya.deskripsi,
+            'user': karya.user.username,
+            'harga': karya.harga,
+            'tanggal': karya.tanggal.strftime('%d %B %Y'),
+            'gambar': karya.gambar.url,
+            'sudah_dibeli': transaksi.exists()
+        })
+    return HttpResponse(json.dumps(lst), content_type='application/json')
+
+@csrf_exempt
+def post_beli_karya_json(request):
+    if request.method == 'POST':
+        requestData = json.loads(request.body.decode("utf-8"))
+        data = QueryDict("", mutable=True)
+        data.update(requestData)
+        karya = Karya.objects.get(pk = int(data["id"]))
+        transaksi = Transaksi(karya=karya)
+        transaksi.save()
+    return HttpResponseRedirect("/beli_karya")
